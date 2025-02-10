1a2493b60dfcc09d6b37c560e89131402153491f
santosh <santosh8978kumar@gmail.com>
2025-02-11 01:58:27 +0530

    file added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function moveRandomE1(elm) {
+function moveRandomEl(elm) {
     elm.style.position = "absolute";
     elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
     elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
@@ -6,14 +6,23 @@ function moveRandomE1(elm) {
 
 const moveRandom = document.querySelector("#move-Random");
 
-// Event for desktop
-moveRandom.addEventListener("mouseenter", function (e) {
-    moveRandomE1(e.target);
+if (moveRandom) {
+    // Event for desktop
+    moveRandom.addEventListener("mouseenter", function (e) {
+        moveRandomEl(e.target);
+    });
+
+    // Event for mobile
+    moveRandom.addEventListener("touchstart", function (e) {
+        e.preventDefault(); // Prevents unintended scrolling
+        moveRandomEl(e.target);
+    });
+}
+
+// Background music when clicking "No"
+document.getElementById("noBtn").addEventListener("click", function () {
+    let audio = document.getElementById("bgMusic");
+    if (audio) {
+        audio.play();
+    }
 });
-
-// Event for mobile
-moveRandom.addEventListener("touchstart", function (e) {
-    moveRandomE1(e.target);
-});
-
-
